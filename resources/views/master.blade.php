9d855ef23af134388ee7470518b4ac59ba30d01b
Dams168 <adamrohman2003@gmail.com>
2023-11-02 23:28:02 +0700

    Footer, responsive and Fix

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -3,14 +3,117 @@
 <head>
    <meta charset="utf-8">
    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.15/dist/tailwind.min.css">
     <title>@yield('title')</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
-      @yield('container')
+    <div id="content" class="md:ml-80 mt-5 min-h-screen">
+        <h1 class="z-0 m-auto text-4xl py-4 max-[768px]:text-center min-[768px]:text-left">@yield('title')</h1>
+        @yield('container')
+    </div>
+    <!-- sidebar toggle button-->
+    <button id="sidebarToggle" class="md:hidden fixed top-0 right-0 bg-slate-200 w-12 h-12 text-black text-2xl flex items-center justify-center">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16"></path>
+        </svg>
+    </button>
+    <!-- sidebar -->
+    <div id="sidebar-section" class="min-h-full md:w-72 lg:w-1/6 xl:w-1/6 md:flex-shrink-0 md:fixed hidden md:block">
+        <div id="sidebar" class="fixed h-screen top-0 left-0 bg-slate-800 w-72">
+            <nav role="navigation" class="pl-4 pt-8 mt-4 z-10">
+                <div class="mt-4 relative overflow-hidden">
+                    <h1 class="name text-white fixed top-2 text-4xl">WangKas</h1>
+                    <ul class="nas-list" id="nav-list">
+                        <li class="nav-item text-xl  p-2 rounded-l-xl text-white hover:bg-slate-400 hover:text-blue-800">
+                            <a href="/dashboard" class="flex gap-5">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z" />
+                                </svg>
+                                Dashboard
+                            </a>
+                        </li>
+                        <li class="nav-item text-xl p-2 rounded-l-xl text-white hover:bg-slate-400 hover:text-blue-800">
+                            <a href="/pemasukan" class="flex gap-5">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-.125-.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+                                </svg>
+                                Pemasukan
+                            </a>
+                        </li>
+                        <li class="nav-item text-xl p-2 rounded-l-xl text-white hover:bg-slate-400 hover:text-blue-800">
+                            <a href="/pengeluaran" class="flex gap-5">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m6.75 12H9m1.5-12H5.625c-.621 0-.125-.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+                                </svg>
+                                Pengeluaran
+                            </a>
+                        </li>
+                        <li class="nav-item text-xl p-2 rounded-l-xl text-white hover:bg-slate-400 hover:text-blue-800">
+                            <a href="/laporan" class="flex gap-5">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m3.75 12H9m1.5-12H5.625c-.621 0-.125-.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+                                </svg>
+                                Laporan
+                            </a>
+                        </li>
+                        <li class="nav-item text-xl p-2 rounded-l-xl text-white hover:bg-slate-400 hover:text-blue-800">
+                            <a href="/pengguna" class="flex gap-5">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                                </svg>
+                                User
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
+
+
+    <footer class="mt-auto py-2 px-6 sm:px-6 md:ml-72 w-auto bg-slate-300 ">
+        <div class="text-center">
+          <div>
+            <a class="flex-none text-xl font-semibold text-black" href="#" aria-label="Brand">WangKas</a>
+          </div>
+          <div class="mt-3">
+            <p class="text-gray-500">©2023 Group-G. IFA-21. All rights reserved.</p>
+          </div>
+
+          <div class="mt-3 space-x-2">
+            <a class="inline-flex justify-center items-center w-10 h-10 text-center text-gray-500 hover:bg-gray-100 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white transition dark:text-gray-500 dark:hover:text-gray-200 dark:hover:bg-gray-800" href="https://github.com/Dams168">
+              <svg class="w-3.5 h-3.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+              </svg>
+            </a>
+
+            <a class="inline-flex justify-center items-center w-10 h-10 text-center text-gray-500 hover:bg-gray-100 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white transition dark:text-gray-500 dark:hover:text-gray-200 dark:hover:bg-gray-800" href="https://github.com/FiqriAsshidiq">
+                <svg class="w-3.5 h-3.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+                </svg>
+              </a>
+
+              <a class="inline-flex justify-center items-center w-10 h-10 text-center text-gray-500 hover:bg-gray-100 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white transition dark:text-gray-500 dark:hover:text-gray-200 dark:hover:bg-gray-800" href="https://github.com/syaniaaa">
+                <svg class="w-3.5 h-3.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+                </svg>
+              </a>
+          </div>
+        </div>
+      </footer>
+
+      <script>
+        // for Sidebar Responsive In    768px
+        const sidebarSection = document.getElementById('sidebar-section');
+
+        sidebarToggle.addEventListener('click', () => {
+            sidebarSection.classList.toggle('hidden');
+        });
+    </script>
+
    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
    </script>
-   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
--- resources/views/pengguna/Data-Pengguna.blade.php
+++ resources/views/pengguna/Data-Pengguna.blade.php
@@ -1,24 +1,24 @@
 @extends('master')
 @section('title','Data Pengguna')
 @section('container')
-<div class="container">
-    <div class="my-4 col-12">
-    <h1 class="float-left">Data Pengguna</h1>
-    </div>
-    <table class="table text-center">
-            <th>No</th>        
-           <th>NPM</th>
-            <th>Nama</th>
-            <th>Email</th>
-
+<div class="container mx-auto">
+    <div class="my-4 col-12"></div>
+    <table class="w-11/12 table-fixed border-collapse text-center mx-auto ">
+        <thead class="bg-blue-400">
+            <tr>
+                <th class="w-1/12 px-4 py-2">No</th>
+                <th class="w-2/12 px-4 py-2">NPM</th>
+                <th class="w-1/4 px-4 py-2">Nama</th>
+                <th class="w-1/4 px-4 py-2">Email</th>
+            </tr>
         </thead>
         <tbody>
             @foreach ($pengguna as $key => $usr)
-            <tr>
-                <td>{{ $key + 1 }}</td>
-                <td>{{ $usr['npm'] }}</td>
-                <td>{{ $usr['nama'] }}</td>
-                <td>{{ $usr['email'] }}</td>
+            <tr class="border-b border-gray-300">
+                <td class="px-4 py-2">{{ $key + 1 }}</td>
+                <td class="px-4 py-2">{{ $usr['npm'] }}</td>
+                <td class="px-4 py-2">{{ $usr['nama'] }}</td>
+                <td class="px-4 py-2">{{ $usr['email'] }}</td>
             </tr>
             @endforeach
         </tbody>
